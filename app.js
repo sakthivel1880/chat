e1bde7edc8a397ee14fd63720ae994fb745e11e6
sakthivel1880 <54572214+sakthivel1880@users.noreply.github.com>
2020-01-21 16:48:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
-var port = process.env.PORT || 5000;
+app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/node_modules'));
+var port = process.env.PORT || 8080;
 app.locals.rootpath = "http://90.0.1.73/chat";
 app.use(
     bodyParser.urlencoded({
